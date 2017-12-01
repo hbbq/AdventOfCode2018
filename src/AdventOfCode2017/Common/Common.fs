a41e421cd316e4c4f31b1044948291d0d035faba
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-12-01 09:48:43 +0100

    1:1

---
+++ src/AdventOfCode2017/Common/Common.fs
@@ -0,0 +1,41 @@
+﻿module Common
+
+let Chars =
+    string >> (fun e -> seq [for c in e -> c])
+
+let IsNumeric v = 
+    fst (System.Int32.TryParse(string(v)))
+
+let Digits = 
+    string >> Chars >> Seq.where IsNumeric >> Seq.map (string >> int)
+
+let DigitSum =
+    string >> Digits >> Seq.sum
+
+let rec InfSeq start inc = seq {
+    yield start
+    yield! InfSeq (start + inc) inc
+}
+
+let IsPrime n =
+    let f = float n
+    f = 2.0 
+    || (
+        f % 2.0 > 0.0
+        &&
+        seq {3.0..2.0..sqrt f} |> Seq.tryFind (fun e -> f % e = 0.0) |> (=) None
+    )
+
+let Primes =
+    let s = seq {
+        yield 2
+        yield! InfSeq 3 2
+    }
+    s |> Seq.filter IsPrime
+
+let rec Factorize (n:int) = seq {
+    if n > 1 then
+        let f = Primes |> Seq.find (fun e -> n % e = 0) |> int
+        yield f
+        yield! Factorize (n / f)
+}
---
+++ src/AdventOfCode2017/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+
+    let x = Day1.Problem1
+    
+    printfn "%A" x
+    let a =System.Console.ReadKey()
+    0 // return an integer exit code
