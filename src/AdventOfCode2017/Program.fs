40d943137131de0aa9bbe7537503cf14cbc80dbd
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-12-04 13:08:38 +0100

    Clean

--- src/AdventOfCode2017/Program.fs
+++ src/AdventOfCode2017/Program.fs
@@ -1,11 +1,10 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main argv = 
 
     let x = Day4.Problem2
-    
+        
     printfn "%A" x
-    let a =System.Console.ReadKey()
-    0 // return an integer exit code
+
+    System.Console.ReadKey() |> ignore
+
+    0
