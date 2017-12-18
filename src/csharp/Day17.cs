7588f6c7203837463831f4a4c0cb4c7fef309d16
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-12-18 12:43:16 +0100

    17:1 17:2

--- src/csharp/Day17.cs
+++ src/csharp/Day17.cs
@@ -17,14 +17,44 @@ namespace csharp
         public static int Problem1()
         {
 
-            return 171;
+            var input = 355;
+
+            var count = 2017;
+
+            var list = new List<int> { 0 };
+
+            var pos = 0;
+
+            for(var num = 1; num <= count; num++)
+            {
+                pos = (pos + input) % num;
+                list.Insert(pos + 1, num);
+                pos++;
+            }
+
+            return list[pos + 1];
 
         }
 
         public static int Problem2()
         {
 
-            return 172;
+            var input = 355;
+
+            long count = 50_000_000;
+                        
+            var pos = 0;
+            var last1 = 0;
+
+            for (var num = 1; num <= count; num++)
+            {
+                if (num % 100_000 == 0) Trace.WriteLine(num);
+                pos = (pos + input) % num;
+                pos++;
+                if (pos == 1) last1 = num;
+            }
+
+            return last1;
 
         }
         
