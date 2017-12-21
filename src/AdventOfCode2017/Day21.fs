8cbebd7cacff86357cd7bdc1a38eaa451491b136
Henrik Bergqvist <henrik.bergqvist@alektumgroup.com>
2017-12-21 10:55:12 +0100

    21:1 21:2

--- src/AdventOfCode2017/Day21.fs
+++ src/AdventOfCode2017/Day21.fs
@@ -1,7 +1,7 @@
 ﻿module Day21
 
 let Problem1 =
-    csharp.Day21.Problem1()
+    csharp.Day21.Problem1(LargeInputs.Day21)
 
 let Problem2 =
-    csharp.Day21.Problem2()
+    csharp.Day21.Problem2(LargeInputs.Day21)
