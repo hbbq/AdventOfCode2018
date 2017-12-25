7f7ff2706ad88435aed8a88239da5c9e7ca926b4
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2017-12-25 22:45:48 +0100

    23:1 23:2

--- src/AdventOfCode2017/Day23.fs
+++ src/AdventOfCode2017/Day23.fs
@@ -1,7 +1,7 @@
 ﻿module Day23
 
 let Problem1 =
-    csharp.Day23.Problem1()
+    csharp.Day23.Problem1(LargeInputs.Day23)
 
 let Problem2 =
-    csharp.Day23.Problem2()
+    csharp.Day23.Problem2(LargeInputs.Day23)
